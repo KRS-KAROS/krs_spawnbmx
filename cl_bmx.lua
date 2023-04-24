896920cba0a013fa94d420c958969106e69e7454
KRS <131356071+KRS-KAROS@users.noreply.github.com>
2023-04-24 20:27:45 +0200

    Add files via upload

--- cl_bmx.lua
+++ cl_bmx.lua
@@ -1,24 +1,28 @@
 ESX = exports["es_extended"]:getSharedObject()
 
 
+
 local options = {
+  
     {
         name = 'ox:option1',
         icon = 'fa-solid fa-bicycle',
         label = 'Prendi bmx',
         onSelect = function(data)
-            SetEntityAsMissionEntity(data.entity, true, true)
-            DeleteEntity(data.entity)
-            TriggerServerEvent('eliminabici')
-        end, 
+        SetEntityAsMissionEntity(data.entity, true, true)
+        DeleteEntity(data.entity)
+        TriggerServerEvent('eliminabici')
+    end, 
         canInteract = function(entity, distance, coords, name, bone)
             return not IsEntityDead(entity)
         end
     }
 }
 
+local models = { 1131912276 }
+local optionsNames = { 'ox:option1'}
 
-exports.ox_target:addGlobalVehicle(options)
+exports.ox_target:addModel(models, options)
 
 
 RegisterNetEvent("spawnbmx")
@@ -32,10 +36,11 @@ AddEventHandler("spawnbmx", function()
     end
     local forward = GetEntityForwardVector(playerPed)
     local spawnCoords = coords + forward * 2.0
+    
     local bmx = CreateVehicle(GetHashKey("bmx"), spawnCoords.x, spawnCoords.y, spawnCoords.z, GetEntityHeading(playerPed), true, false)
-
+    
     SetPedIntoVehicle(playerPed, bmx, -1)
-    SetVehicleNumberPlateText(bmx, "BMX" .. math.random(1000, 9999))
+    -- SetVehicleNumberPlateText(bmx, "BMX" .. math.random(1000, 9999))
     TaskWarpPedIntoVehicle(playerPed, bmx, -1)
 end)
 
